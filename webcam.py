e87f4cec01dccf7b653095418d00228abdabd437
Sk <techjk021@gmail.com>
2024-07-31 21:10:13 +0530

    Added files

--- webcam.py
+++ webcam.py
@@ -5,7 +5,7 @@ video_capture =cv2.VideoCapture(0)
 while True:
     _,  img = video_capture.read()
     cv2.imshow("face detection",img)
-    if cv2.waitKey(30) & 0xFF == ord('g'):
+    if (cv2.waitKey(30) & 0xFF) == 27:
         break
+    # cv2.imshow("face detection")   
 video_capture.release()
-cv2.imshow("face detection")   
